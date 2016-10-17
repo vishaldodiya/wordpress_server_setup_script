e869cae1eb7b2bbf40d3069dbd75de2b36c468a1
vishaldodiya <13bce026@nirmauni.ac.in>
2016-10-17 23:42:27 +0530

    added php mysql and apache server checking

--- bash.sh
+++ bash.sh
@@ -1,7 +1,36 @@
 #Need to access server through SSH
 if ssh $1@$2;
 then
+    apache=$(apt-chace policy apache2 | grep "Installed" | awk -F' ' '{print $2}')
     
+    if $apache != "(none)";
+    then 
+        echo "Apache2 is already installed"
+    else
+        sudo apt install apache2
+    fi
+
+    php=$(apt-cache policy php | grep "Installed" | awk -F' ' '{print $2}')
+
+    if $php != "(none)";
+    then
+        echo "Php is already installed"
+    else
+        sudo apt install php
+    fi
+
+    mysql=$(apt-cache policy mysql | grep "Installed" | awk -F' ' '{print $2}')
+
+    if $mysql != "(none)";
+    then 
+        echo "Mysql is already installed"
+    else
+        sudo apt install mysql-server
+    fi
+    
+    echo -n "Enter your Domain name"
+    read domain
+
 else
     echo "Server Can't be reached'"
 fi
