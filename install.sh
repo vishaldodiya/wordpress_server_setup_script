2bab5ddc8ab767cab12d962801b8eb4658d18782
vishaldodiya <13bce026@nirmauni.ac.in>
2016-10-26 15:50:38 +0530

    .gz file for man page

--- install.sh
+++ install.sh
@@ -1,48 +1,40 @@
 if [ -z "$1" -o -z "$2" ]
 then
     echo "Enter valid Argument"
-    echo "Enter command in specified format: wordpress_server <username> <ip address>"
+    echo "Enter command in specified format: sh install.sh <username> <ip address>"
 else
 
 sudo cp wordpress_server /bin/wordpress_server
-sudo cp setup_script.sh /bin/setup_script.sh
+sudo cp setup_script.sh /usr/local/bin/setup_script.sh
 
 sudo chmod +x /bin/wordpress_server
 
-sudo tar -czvf wordpress_server.gz wordpress_server.man
+sudo mv wordpress_server.1.gz /usr/share/man/man1/
 
-sudo mv wordpress_server.tar.gz /usr/share/man/man1/
+sudo apt install openssh-client
 
 ssh-keygen -t rsa
+ssh-add
 
-ssh $1@$2
+ssh $1@$2 << EOF
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys
+EOF
 
-#if test -f .ssh/authorized_keys;
-#then
-#    echo 'authorized key exist'
-#else 
-    mkdir .ssh
-    touch .ssh/authorized_keys
-    exit
-#fi
-
-cat ~/.ssh/id_rsa.pub | ssh -l $1 $2 "cat >> .ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub | ssh $1@$2 "cat >> .ssh/authorized_keys"
 
 #echo $(whoami) "All=(ALL) NOPASSWD: ALL" | ssh -l $1 $2 "sudo cat >> /etc/sudoers"
-
-ssh -l $1@$2
-
-while true; do
-    read -p "Do you wish to change the sudoers script to make your script automated?" yn
+ssh -l $1 $2 << EOF
+while tr ue; do
+    read -p "Do you wish to change the sudoers script to make your script automated [Y/N]?" yn
     case $yn in
-        [Yy]* ) echo "Insert <username> ALL=(ALL) NOPASSWD: ALL at the end of the file" 
-                sudo visudo; 
-                break;;
-        [Nn]* ) exit;;
+        [Yy]* ) sudo visudo;break;;
+        [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
- 
+EOF
 #IF condition for automation
 
 #echo "Change sudoers file to make automation in sudo command"
@@ -52,4 +44,3 @@ done
 echo "Initial Server Setup done Now you Can run wordpress_server command to setup wordpress file on your server"
 
 fi
-
