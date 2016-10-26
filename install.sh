f5b928bd39d092daf9fd7c56158392dd0c3aa4a9
vishaldodiya <13bce026@nirmauni.ac.in>
2016-10-26 19:22:17 +0530

    dangerous changes

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ sudo cp setup_script.sh /usr/local/bin/setup_script.sh
 
 sudo chmod +x /bin/wordpress_server
 
-sudo mv wordpress_server.1.gz /usr/share/man/man1/
+sudo cp wordpress_server.1.gz /usr/share/man/man1/
 
-sudo apt install openssh-client
+sudo apt install -y openssh-client
 
 ssh-keygen -t rsa
 ssh-add
@@ -24,17 +24,39 @@ EOF
 
 cat ~/.ssh/id_rsa.pub | ssh $1@$2 "cat >> .ssh/authorized_keys"
 
+#ssh -l $1 $2 << EOF
 #echo $(whoami) "All=(ALL) NOPASSWD: ALL" | ssh -l $1 $2 "sudo cat >> /etc/sudoers"
-ssh -l $1 $2 << EOF
-while tr ue; do
-    read -p "Do you wish to change the sudoers script to make your script automated [Y/N]?" yn
-    case $yn in
-        [Yy]* ) sudo visudo;break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-EOF
+#echo "To Automate our script you need to insert below command in sudoers file."
+#echo "<your username> ALL=(ALL) NOPASSWD: ALL"
+
+#echo "Do you wish to change the sudoers script to make your script automated?"
+#select yn in "Yes" "No"; do
+#case $yn in
+#Yes ) sudo visudo; break;;
+#No ) break;;
+#esac
+#done
+#EOF
+echo "============================================================================"
+echo "|| To Automate our script you need to insert below command in sudoers file.||"
+echo "============================================================================"
+echo "|| <your username> ALL=(ALL) NOPASSWD: ALL                                ||"
+echo "============================================================================"
+ssh -t $1@$2 'sudo visudo'
+#ssh -l $1 $2 << 'EOF'
+#    sudo visudo
+#EOF
+
+#ssh -l $1 $2 << EOF
+#while true; do
+#read -p "Do you wish to change the sudoers script to make your script automated [Y/N]?" yn
+#case $yn in
+#[Yy]* ) sudo visudo; break;;
+#[Nn]* ) break;;
+#* ) echo "Please answer yes or no.";;
+#esac
+#done
+#EOF
 #IF condition for automation
 
 #echo "Change sudoers file to make automation in sudo command"
--- setup_script.sh
+++ setup_script.sh
@@ -4,7 +4,7 @@ if test "$apache" != "(none)";
 then 
     echo "Apache2 is already installed"
 else
-    sudo apt install apache2
+    sudo apt install -y apache2
 fi
 
 #checking for PHP
@@ -15,7 +15,7 @@ if test "$php" != "(none)";
 then
     echo "Php is already installed"
 else
-    sudo apt install php
+    sudo apt install -y php
 fi
 
 #checking for mysql
@@ -26,7 +26,7 @@ if test "$mysql" != "(none)";
 then 
     echo "Mysql is already installed"
 else
-    sudo apt install mysql-server
+    sudo apt install -y mysql-server
 fi
 
 #checking for Nginx
@@ -38,44 +38,44 @@ then
     echo "Nginx is already installed"
 else
     sudo apt-get update
-    sudo apt-get install nginx
-    ufw_status=$(sudo ufw status)
+    sudo apt-get install -y nginx
+#    ufw_status=$(sudo ufw status)
 
-    if test "$ufw_status" != "Status: inactive";
-    then
-        echo "ufw is active"
-        nginx_status=$(sudo ufw status | grep "^Nginx HTTP" | awk -F' ' '{print $3}')
-        nginx_v6_status=$(sudo ufw status | grep "^Nginx HTTP (v6)" | awk -F' ' '{print $4}')
-        
-        if $nginx_status = deny || $nginx_v6_status = deny;
-         then
-              sudo ufw allow 'Nginx HTTP'
-        else
-            echo "Nginx completely installed"
-        fi
-            
-    else
-        sudo ufw enable
-        sudo ufw allow 'Nginx HTTP'
-    fi
+#    if test "$ufw_status" != "Status: inactive";
+#    then
+#        echo "ufw is active"
+#        nginx_status=$(sudo ufw status | grep "^Nginx HTTP" | awk -F' ' '{print $3}')
+#        nginx_v6_status=$(sudo ufw status | grep "^Nginx HTTP (v6)" | awk -F' ' '{print $4}')
+#        
+#        if $nginx_status = deny || $nginx_v6_status = deny;
+#         then
+#              sudo ufw allow 'Nginx HTTP'
+#        else
+#            echo "Nginx completely installed"
+#       fi
+#            
+#    else
+#        sudo ufw enable
+#        sudo ufw allow 'Nginx HTTP'
+#    fi
             
 fi
 
 
 
 #reading Domain name
-    
-echo -n "Enter your Domain name"
-read domain
+#read -p "Enter your Domain name:" domain    
+#echo -n "Enter your Domain name"
+#read domain
 
     #making domain directory
 
-sudo mkdir -p /var/www/$domain/public_html
+sudo mkdir /var/www/$3/public_html
     
 #granting permission
 
-sudo chown -R www-data:www-data /var/www/$domain/public_html
-sudo chmod -R 755 /var/www/$domain/public_html
+sudo chown www-data:www-data /var/www/$3/public_html
+sudo chmod 755 /var/www/$3/public_html
 
 sudo echo "<html>\
                <head>\
@@ -87,11 +87,11 @@ sudo echo "<html>\
                <body>\
                   <h1>It Worked!!! :) :)</h1>\
                 </body>\
-            </html>" > /var/www/$domain/public_html/index.html
+            </html>" > /var/www/$3/public_html/index.html
 
-sudo echo "DocumentRoot /var/www/$domain/public_html\
-            ServerName www.$domain\
-            ServerAlias $domain\
+sudo echo "DocumentRoot /var/www/$3/public_html\
+            ServerName www.$3\
+            ServerAlias $3\
             ErrorLog \${APACHE_LOG_DIR}/error.log\
             CustomLog \${APACHE_LOG_DIR}/access.log" > /etc/apache2/sites-available/$domain.conf
 
@@ -105,12 +105,12 @@ wget -c http://wordpress.org/latest.tar.gz
 
 tar -xzvf latest.tar.gz
 
-sudo rsync -av wordpress/* /var/www/$domain/
+sudo rsync -av wordpress/* /var/www/$3/
 
 #setting up database
 
 mysql -u root -p
 
-CREATE DATABASE $domain\_db;
+CREATE DATABASE $3\_db;
 
     
