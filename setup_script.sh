a9a32b8310d8c0076b0205490308db61490df538
vishaldodiya <13bce026@nirmauni.ac.in>
2016-10-29 18:09:22 +0530

    final change

--- setup_script.sh
+++ setup_script.sh
@@ -93,7 +93,7 @@ sudo printf " server {
 
 #make symbolic link between sites-available and sites-enabled example.com file
 
-sudo ln -s /etc/nginx/sites-available/$1/etc/nginx/sites-enabled/$1
+sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
 
 
 sudo rm /etc/nginx/sites-enabled/default
